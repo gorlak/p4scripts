8196b15e06f248de6743dfc4966e5ddc42fb9da6
Geoff Evans <geoff@flummoxed.org>
2011-11-03 13:27:59 -0700

    initial commit

---
+++ p4top.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import time
+
+import optparse
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interval", dest="interval", default=1.0,
+                  help="time interval in seconds")
+(options, args) = parser.parse_args()
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+import P4
+p4 = P4.P4()
+
+try:
+	p4.connect()
+  
+	while( 1 ):
+
+		os.system('cls')
+
+		result = p4.run('info')
+		for key in result[0]:
+			print(key, '=', result[0][key])
+
+		print("") #newline
+
+		results = p4.run('counters')
+		for result in results:
+			print( 'Counter \'{}\': {}'.format( result['counter'], result['value'] ) )
+
+		print("") #newline
+
+		results = p4.run('monitor', 'show')
+		#pp.pprint(results)
+
+		results = sorted(results, key=lambda result: result['time'], reverse=True)
+		#pp.pprint(results)
+
+		for result in results:
+			print( '{}/{:5}: {} - {:20} - {}'.format( result['status'], result['id'], result['time'], result['user'], result['command'] ) )
+
+		try:
+			time.sleep( float( options.interval ) )
+		except KeyboardInterrupt:
+			exit(0)
+
+	p4.disconnect()
+
+except P4.P4Exception:
+	for e in p4.errors:
+		print(e)
