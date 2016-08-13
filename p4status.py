e89bdb4c8b2c72fa4ef9d9bd41e5e8fa91a9f667
Geoff Evans <geoff@flummoxed.org>
2016-08-12 20:01:00 -0700

    add junction/symlink support

--- p4status.py
+++ p4status.py
@@ -1,3 +1,4 @@
+import locale
 import os
 import re
 import stat
@@ -12,187 +13,390 @@ parser.add_option( "-a", "--clean_added", dest="clean_added", action="store_true
 parser.add_option( "-e", "--clean_edited", dest="clean_edited", action="store_true", default=False, help="clean: revert files that are opened for edit" )
 parser.add_option( "-m", "--clean_missing", dest="clean_missing", action="store_true", default=True, help="clean: restore files that are missing locally" )
 parser.add_option( "-x", "--clean_extra", dest="clean_extra", action="store_true", default=True, help="clean: delete files that are unknown or deleted at #have" )
+parser.add_option( "-d", "--clean_dirs", dest="clean_dirs", action="store_true", default=True, help="clean: delete empty directories" )
 ( options, args ) = parser.parse_args()
 
 import pprint
 pp = pprint.PrettyPrinter( indent=4 )
+
+if os.name != "nt":
+    print( "Not tested outside of windows\n" )
+    exit( 1 )
+
+#
+# https://eklausmeier.wordpress.com/2015/10/27/working-with-windows-junctions-in-python/
+#
+
+from ctypes import *
+from ctypes.wintypes import *
+
+kernel32 = WinDLL('kernel32')
+LPDWORD = POINTER(DWORD)
+UCHAR = c_ubyte
+
+GetFileAttributesW = kernel32.GetFileAttributesW
+GetFileAttributesW.restype = DWORD
+GetFileAttributesW.argtypes = (LPCWSTR,) #lpFileName In
+
+INVALID_FILE_ATTRIBUTES = 0xFFFFFFFF
+FILE_ATTRIBUTE_REPARSE_POINT = 0x00400
+
+CreateFileW = kernel32.CreateFileW
+CreateFileW.restype = HANDLE
+CreateFileW.argtypes = (LPCWSTR, #lpFileName In
+                        DWORD,   #dwDesiredAccess In
+                        DWORD,   #dwShareMode In
+                        LPVOID,  #lpSecurityAttributes In_opt
+                        DWORD,   #dwCreationDisposition In  
+                        DWORD,   #dwFlagsAndAttributes In
+                        HANDLE)  #hTemplateFile In_opt
+
+CloseHandle = kernel32.CloseHandle
+CloseHandle.restype = BOOL
+CloseHandle.argtypes = (HANDLE,) #hObject In
+
+INVALID_HANDLE_VALUE = HANDLE(-1).value
+OPEN_EXISTING = 3
+FILE_FLAG_BACKUP_SEMANTICS = 0x02000000
+FILE_FLAG_OPEN_REPARSE_POINT = 0x00200000
+
+DeviceIoControl = kernel32.DeviceIoControl
+DeviceIoControl.restype = BOOL
+DeviceIoControl.argtypes = (HANDLE,  #hDevice In
+                            DWORD,   #dwIoControlCode In
+                            LPVOID,  #lpInBuffer In_opt
+                            DWORD,   #nInBufferSize In
+                            LPVOID,  #lpOutBuffer Out_opt
+                            DWORD,   #nOutBufferSize In
+                            LPDWORD, #lpBytesReturned Out_opt
+                            LPVOID)  #lpOverlapped Inout_opt
+
+FSCTL_GET_REPARSE_POINT = 0x000900A8
+IO_REPARSE_TAG_MOUNT_POINT = 0xA0000003
+IO_REPARSE_TAG_SYMLINK = 0xA000000C
+MAXIMUM_REPARSE_DATA_BUFFER_SIZE = 0x4000
+
+class GENERIC_REPARSE_BUFFER(Structure):
+    _fields_ = (('DataBuffer', UCHAR * 1),)
+
+class SYMBOLIC_LINK_REPARSE_BUFFER(Structure):
+    _fields_ = (('SubstituteNameOffset', USHORT),
+                ('SubstituteNameLength', USHORT),
+                ('PrintNameOffset', USHORT),
+                ('PrintNameLength', USHORT),
+                ('Flags', ULONG),
+                ('PathBuffer', WCHAR * 1))
+    @property
+    def PrintName(self):
+        arrayt = WCHAR * (self.PrintNameLength // 2)
+        offset = type(self).PathBuffer.offset + self.PrintNameOffset
+        return arrayt.from_address(addressof(self) + offset).value
+
+class MOUNT_POINT_REPARSE_BUFFER(Structure):
+    _fields_ = (('SubstituteNameOffset', USHORT),
+                ('SubstituteNameLength', USHORT),
+                ('PrintNameOffset', USHORT),
+                ('PrintNameLength', USHORT),
+                ('PathBuffer', WCHAR * 1))
+    @property
+    def PrintName(self):
+        arrayt = WCHAR * (self.PrintNameLength // 2)
+        offset = type(self).PathBuffer.offset + self.PrintNameOffset
+        return arrayt.from_address(addressof(self) + offset).value
+
+class REPARSE_DATA_BUFFER(Structure):
+    class REPARSE_BUFFER(Union):
+        _fields_ = (('SymbolicLinkReparseBuffer', 
+                        SYMBOLIC_LINK_REPARSE_BUFFER),
+                    ('MountPointReparseBuffer', 
+                        MOUNT_POINT_REPARSE_BUFFER),
+                    ('GenericReparseBuffer', 
+                        GENERIC_REPARSE_BUFFER))
+    _fields_ = (('ReparseTag', ULONG),
+                ('ReparseDataLength', USHORT),
+                ('Reserved', USHORT),
+                ('ReparseBuffer', REPARSE_BUFFER))
+    _anonymous_ = ('ReparseBuffer',)
+
+def isjunction(path):
+    result = GetFileAttributesW(path)
+    if result == INVALID_FILE_ATTRIBUTES:
+        raise WinError()
+    return bool(result & FILE_ATTRIBUTE_REPARSE_POINT)
+
+def readjunction(path):
+    reparse_point_handle = CreateFileW(path, 
+                                    0,
+                                    0,
+                                    None,
+                                    OPEN_EXISTING,
+                                    FILE_FLAG_OPEN_REPARSE_POINT |
+                                    FILE_FLAG_BACKUP_SEMANTICS,
+                                    None)
+    if reparse_point_handle == INVALID_HANDLE_VALUE:
+        raise WinError()
+    target_buffer = c_buffer(MAXIMUM_REPARSE_DATA_BUFFER_SIZE)
+    n_bytes_returned = DWORD()
+    io_result = DeviceIoControl(reparse_point_handle, 
+                                FSCTL_GET_REPARSE_POINT, 
+                                None, 0,
+                                target_buffer, len(target_buffer),
+                                byref(n_bytes_returned),
+                                None)
+    CloseHandle(reparse_point_handle)
+    if not io_result:
+        raise WinError()
+    rdb = REPARSE_DATA_BUFFER.from_buffer(target_buffer)        
+    if rdb.ReparseTag == IO_REPARSE_TAG_SYMLINK:
+        return rdb.SymbolicLinkReparseBuffer.PrintName
+    elif rdb.ReparseTag == IO_REPARSE_TAG_MOUNT_POINT:
+        return rdb.MountPointReparseBuffer.PrintName
+    raise ValueError("not a link")
+
+#
+# main
+#
 
 import P4
 p4 = P4.P4()
 
 try:
 
-  if os.name != "nt":
-    print( "Not tested outside of windows\n" )
-    exit( 1 )
+    #
+    # connect and setup p4
+    #
 
-  p4.connect()
+    p4.connect()
 
-  client = p4.fetch_client()
-  clientMap = P4.Map( client[ 'View' ] )
-  clientRoot = client[ 'Root' ]
+    # handle non-unicode servers by marshalling raw bytes to local encoding
+    if not p4.server_unicode:
+        p4.encoding = 'raw'
+    def p4MarshalString( data ):
+        if p4.server_unicode:
+            return data
+        else:
+            return data.decode( locale.getpreferredencoding() )
 
-  # handle the p4config file special as its always hanging out, if it exists
-  p4configFile = p4.p4config_file
-  if p4configFile != None:
-    p4configFile = p4configFile.lower().replace( os.getcwd().lower() + '\\', '' )
+    # handle the p4config file special as its always hanging out, if it exists
+    p4configFile = p4.p4config_file
+    if p4configFile != None:
+        p4configFile = p4configFile.lower().replace( os.getcwd().lower() + '\\', '' )
 
-  if ( clientRoot[-1] != '\\' ) and ( clientRoot[-1] != '/' ):
-    clientRoot += '/'
+    # setup client
+    client = p4.fetch_client()
+    clientMap = P4.Map( client[ 'View' ] )
+    clientRoot = client[ 'Root' ]
 
-  clientSlashesFixed = re.sub( r'\\', r'\\\\', clientRoot )
+    if ( clientRoot[-1] != '\\' ) and ( clientRoot[-1] != '/' ):
+        clientRoot += '/'
 
-  def MakeLocalPath( f ):
-    f = clientMap.translate( f )
-    f = re.sub( '//' + re.escape( client[ 'Client' ] ) + '/(.*)', clientSlashesFixed + '\\1', f, 0, re.IGNORECASE )
-    f = re.sub( r'/', r'\\', f )
-    f = re.sub( r'%40', '@', f ) # special handling due to p4 character
-    f = re.sub( r'%23', '#', f ) # special handling due to p4 character
-    f = re.sub( r'%2A', '*', f ) # special handling due to p4 character
-    f = re.sub( r'%25', '%', f ) # special handling due to p4 character
-    return f
+    clientSlashesFixed = re.sub( r'\\', r'\\\\', clientRoot )
 
-  p4Opened = dict ()
-  try:
-    print( "Fetching opened files from p4..." )
-    results = p4.run_opened('-C', client[ 'Client' ], '...')
-    for result in results:
-      f = result[ 'depotFile' ]
-      f = MakeLocalPath( f )
-      f = f.replace( os.getcwd() + '\\', '' )
-      p4Opened[ f.lower() ] = f
-  except KeyboardInterrupt:
-    exit(1) 
+    def p4MakeLocalPath( f ):
+        f = clientMap.translate( f )
+        f = re.sub( '//' + re.escape( client[ 'Client' ] ) + '/(.*)', clientSlashesFixed + '\\1', f, 0, re.IGNORECASE )
+        f = re.sub( r'/', r'\\', f )
+        f = re.sub( r'%40', '@', f ) # special handling due to p4 character
+        f = re.sub( r'%23', '#', f ) # special handling due to p4 character
+        f = re.sub( r'%2A', '*', f ) # special handling due to p4 character
+        f = re.sub( r'%25', '%', f ) # special handling due to p4 character
+        return f
 
-  print( " got " + str( len( p4Opened ) ) + " files from the server")
+    #
+    # query lots of info
+    #
 
-  p4Files = dict ()
-  try:
-    print( "Fetching depot files from p4..." )
-    results = p4.run_files('...#have')
-    for result in results:
-      if result[ 'action' ].find( "delete" ) >= 0:
-        continue
-      f = result[ 'depotFile' ]
-      f = MakeLocalPath( f )
-      p4Files[ f.lower().replace( os.getcwd().lower() + '\\', '' ) ] = f
-  except KeyboardInterrupt:
-    exit(1)
+    p4Opened = dict ()
+    try:
+        print( "Fetching opened files from p4..." )
+        results = p4.run_opened('-C', client[ 'Client' ], '...')
+        for result in results:
+            f = result[ 'depotFile' ]
+            f = p4MakeLocalPath( f )
+            f = f.replace( os.getcwd() + '\\', '' )
+            p4Opened[ f.lower() ] = f
+    except KeyboardInterrupt:
+        exit(1) 
 
-  print( " got " + str( len( p4Files ) ) + " files from the server")
+    print( " got " + str( len( p4Opened ) ) + " files from the server" )
 
-  fsFiles = dict()
-  try:
-    print( "Fetching files from fs..." )
-    for root, dirs, files in os.walk( os.getcwd() ):
-      for name in files:
-        f = os.path.join(root, name)
-        fsFiles[ f.lower().replace( os.getcwd().lower() + '\\', '' ) ] = f
-  except KeyboardInterrupt:
-    exit( 1 )
+    p4Files = dict ()
+    try:
+        print( "Fetching depot files from p4..." )
+        results = p4.run_files('...#have')
+        for result in results:
+            if p4MarshalString( result[ 'action' ] ).find( "delete" ) >= 0:
+                continue
+            f = p4MarshalString( result[ 'depotFile' ] )
+            f = p4MakeLocalPath( f )
+            p4Files[ f.lower().replace( os.getcwd().lower() + '\\', '' ) ] = f
+    except KeyboardInterrupt:
+        exit(1)
 
-  print( " got " + str( len( fsFiles ) ) + " files from the file system")
+    print( " got " + str( len( p4Files ) ) + " files from the server" )
 
-  report = (not options.clean)
+    fsFiles = dict()
+    fsLinks = dict()
+    fsLinkTargets = dict()
+    try:
+        print( "Fetching files from fs..." )
+        for root, dirs, files in os.walk( os.getcwd() ):
+            for name in files:
+                f = os.path.join(root, name)
+                fsFiles[ f.lower().replace( os.getcwd().lower() + '\\', '' ) ] = f
+            for name in dirs:
+                d = os.path.join(root, name)
+                link = None
+                linkTarget = None
+                if os.path.islink( d ):
+                    link = d
+                    linkTarget = os.readlink( d )
+                elif isjunction( d ):
+                    link = d
+                    linkTarget = readjunction( d )
+                if link:
+                    fsLinks[ link.lower().replace( os.getcwd().lower() + '\\', '' ) ] = link
+                    if not os.path.isabs( linkTarget ):
+                        linkTarget = os.path.abspath( os.path.join( os.path.dirname( linkTarget ), linkTarget ) )
+                    if ( linkTarget.lower().startswith( os.getcwd().lower() ) ):
+                        fsLinkTargets[ linkTarget.lower().replace( os.getcwd().lower() + '\\', '' ) ] = linkTarget
+    except KeyboardInterrupt:
+        exit( 1 )
 
-  missing = []
-  for k, v in p4Files.items():
-    if not ( k in fsFiles ):
-      list.append( missing, v )
+    print( " got " + str( len( fsFiles ) ) + " files from the file system" )
 
-  edited = []
-  for k, v in p4Files.items():
-    if ( k in p4Opened ):
-      list.append( edited, v )
+    if len( fsLinks ):
+        print( "  will skip files below " + str( len( fsLinks ) ) + " links:" )
+        for k, v in fsLinks.items():
+            print( "   " + k )
 
-  added = []
-  for k, v in fsFiles.items():
-    if not ( k in p4Files ) and ( k in p4Opened ):
-      list.append( added, v )
+        if len( fsLinkTargets ):
+            print( "  will preserve " + str( len( fsLinkTargets ) ) + " link targets:" )
+            for k, v in fsLinkTargets.items():
+                print( "   " + k )
 
-  extra = []
-  for k, v in fsFiles.items():
-    if k == p4configFile:
-      continue
-    if not ( k in p4Files ) and not ( k in p4Opened ):
-      list.append( extra, v )
+    #
+    # fill out lists of relevant data
+    #
 
-  if report:
+    missing = []
+    for k, v in p4Files.items():
+        if not ( k in fsFiles ):
+            list.append( missing, v )
 
-    clean = True
+    edited = []
+    for k, v in p4Files.items():
+        if ( k in p4Opened ):
+            list.append( edited, v )
 
-    if len( missing ):
-      clean = False
-      print( "\nFiles missing from your disk:" )
-      for f in sorted( missing ):
-        print( f )
+    added = []
+    for k, v in fsFiles.items():
+        if not ( k in p4Files ) and ( k in p4Opened ):
+            list.append( added, v )
 
-    if len( edited ):
-      clean = False
-      print( "\nFiles on your disk open for edit in a changelist:" )
-      for f in sorted( edited ):
-        print( f )
+    extra = []
+    for k, v in fsFiles.items():
+        
+        if k == p4configFile:
+            continue
+        
+        if not ( k in p4Files ) and not ( k in p4Opened ):
+            linked = False
+            for p in sorted( fsLinks.keys() ):
+                if k.startswith( p ):
+                    linked = True
+            if linked:
+                continue;
 
-    if len( added ):
-      clean = False
-      print( "\nFiles on your disk open for add in a changelist:" )
-      for f in sorted( added ):
-        print( f )
+            list.append( extra, v )
 
-    if len( extra ):
-      clean = False
-      print( "\nFiles on your disk not known to the server:" )
-      for f in sorted( extra ):
-        print( f )
+    #
+    # do what we came here to do
+    #
 
-    if clean:
-      print( "\nWorking directory clean!" )
+    report = (not options.clean)
+    if report:
+        clean = True
 
-  elif options.clean:
+        if len( missing ):
+            clean = False
+            print( "\nFiles missing from your disk:" )
+            for f in sorted( missing ):
+                print( f )
 
-    if options.clean_missing:
-      print( "\nSyncing missing files..." )
-      for f in sorted( missing ):
-        p4.run_sync( '-f', f )
-        print( f );
+        if len( edited ):
+            clean = False
+            print( "\nFiles on your disk open for edit in a changelist:" )
+            for f in sorted( edited ):
+                print( f )
 
-    if options.clean_edited:
-      print( "\nReverting edited files..." )
-      for f in sorted( edited ):
-        p4.run_revert( f )
-        print( f );
+        if len( added ):
+            clean = False
+            print( "\nFiles on your disk open for add in a changelist:" )
+            for f in sorted( added ):
+                print( f )
 
-    if options.clean_added:
-      print( "\nCleaning added files..." )
-      for f in sorted( added ):
-        os.chmod( f, stat.S_IWRITE )
-        os.remove( os.path.join( os.getcwd(), f ) )
-        p4.run_revert( f )
-        print( f );
+        if len( extra ):
+            clean = False
+            print( "\nFiles on your disk not known to the server:" )
+            for f in sorted( extra ):
+                print( f )
 
-    if options.clean_extra:
-      print( "\nCleaning extra files..." )
-      for f in sorted( extra ):
-        os.chmod( f, stat.S_IWRITE )
-        os.remove( os.path.join( os.getcwd(), f ) )
-        print( f );
+        if clean:
+            print( "\nWorking directory clean!" )
 
-    print( "\nCleaning empty directories..." )
-    for root, dirs, files in os.walk( os.getcwd(), topdown=False ):
-      for name in dirs:
-        try:
-          d = os.path.join(root, name)
-          os.rmdir( d )
-          d = d.replace( os.getcwd() + '\\', '' )
-          print( d )
-        except WindowsError:
-          pass
+    elif options.clean:
 
-  else:
-    print( "Unknown operation!" )
+        if options.clean_missing:
+            print( "\nSyncing missing files..." )
+            for f in sorted( missing ):
+                p4.run_sync( '-f', f )
+                print( f );
 
-  p4.disconnect()
+        if options.clean_edited:
+            print( "\nReverting edited files..." )
+            for f in sorted( edited ):
+                p4.run_revert( f )
+                print( f );
+
+        if options.clean_added:
+            print( "\nCleaning added files..." )
+            for f in sorted( added ):
+                os.chmod( f, stat.S_IWRITE )
+                os.remove( os.path.join( os.getcwd(), f ) )
+                p4.run_revert( f )
+                print( f );
+
+        if options.clean_extra:
+            print( "\nCleaning extra files..." )
+            for f in sorted( extra ):
+                os.chmod( f, stat.S_IWRITE )
+                os.remove( os.path.join( os.getcwd(), f ) )
+                print( f );
+
+        if options.clean_dirs:
+            print( "\nCleaning empty directories..." )
+            for root, dirs, files in os.walk( os.getcwd(), topdown=False ):
+                for name in dirs:
+                    d = os.path.join(root, name).lower().replace( os.getcwd().lower() + '\\', '' ) 
+                    if d in fsLinks.keys() or d in fsLinkTargets.keys():
+                        continue
+                    try:
+                        os.rmdir( d ) # this will fail for nonempty dirs
+                        d = d.replace( os.getcwd() + '\\', '' )
+                        print( d )
+                    except WindowsError:
+                        pass
+
+    else:
+        print( "Unknown operation!" )
 
 except P4.P4Exception:
-  for e in p4.errors:
-    print( e )
+    for e in p4.errors:
+        print( e )
+
+#
+# shutdown
+#
+
+p4.disconnect()
